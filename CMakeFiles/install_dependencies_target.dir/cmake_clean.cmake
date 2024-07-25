60b528b19e9f1d7caa8e61f7910e7f3b94d6d54b
MargQ <rit.2002014@gmail.com>
2024-07-25 08:42:48 +0700

    remove trash

--- CMakeFiles/install_dependencies_target.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install_dependencies_target"
-  "install_dependencies"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install_dependencies_target.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
