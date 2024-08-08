a8e6d8c8577ec7af4bb18b08360e7cf2bc504424
Абабий Георгий <Georgii200321@gmail.com>
2024-08-08 17:29:28 +0700

    Upd download packages

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-# Обновление списка пакетов
+# Обновление пакетов
 sudo apt update
 
-# Установка необходимых пакетов
-sudo apt install -y libiio-dev libncurses5-dev libboost-program-options-dev libgpm-dev libqt5charts5-dev
+# Функция для проверки пакета
+package_exists() {
+    dpkg -s "$1" &> /dev/null
+}
+
+# Список пакетов
+required_packages=("libiio-dev" "libncurses5-dev" "libboost-program-options-dev" "libgpm-dev" "qtbase5-dev" "qtcharts5-dev")
+
+# Установка пакетов
+for package in "${required_packages[@]}"; do
+    if ! package_exists "$package"; then
+        sudo apt install -y "$package"
+        if package_exists "$package"; then
+            echo "$package успешно установлен."
+        else
+            echo "Ошибка при установке $package."
+        fi
+    else
+        echo "$package уже установлен."
+    fi
+done
 
 echo "Установка завершена."
